0af704c219e4fbdac0404e2d8144844227f9fee8
asitptnk <asitptnk06@gmail.com>
2026-02-04 20:15:51 +0530

    Minimal freestanding Rust binary

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-use core::panic::PanicInfo;
-#![no_std]
-#[panic_handler]
+#![no_std]//don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
 
-fn panic(_info: &PanicInfo) -> ! {
-    loop{}
+use core::panic::PanicInfo;
+
+#[unsafe(no_mangle)] //dont mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    //named `_start` by default
+    loop {}
 }
 
-fn main() {
-    
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop{}
 }
